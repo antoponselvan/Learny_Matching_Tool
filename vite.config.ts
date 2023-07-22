731803f7af4f92ed90ce412a08015a86beee2b64
Berwyn Tan <berwyntan@gmail.com>
2023-07-22 21:34:38 +0800

    vitest setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
 import path from 'path'
 import { defineConfig } from 'vite'
+import type { UserConfig as VitestUserConfigInterface } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// typing for Vitest config
+const vitestConfig: VitestUserConfigInterface = {
+  test: {
+    // includeSource: ["src/**/*.{js,jsx}"]
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/testing/setup/setup.ts'],
+  },
+};
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
+  test: vitestConfig.test
 })
